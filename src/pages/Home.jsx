1079f107d41a3ad420a5f82bc3a76499e1304185
mangemange111 <magnus.enemar@hotmail.com>
2024-11-24 16:59:36 +0100

    styling buttons red/white + navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import './Home.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-    return <div>Home Page</div>;
+    const navigate = useNavigate();
+
+    return (
+        <div className="home-container">
+        <button 
+            className="wine-button white-wine"
+            onClick={() => navigate('/whites')}
+        >
+            Whites
+        </button>
+        
+        <button 
+            className="wine-button red-wine"
+            onClick={() => navigate('/reds')}
+        >
+            Reds
+        </button>
+    </div>
+    );
 }
